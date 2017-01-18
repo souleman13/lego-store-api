b38ca759bfdf4b7c7a3d7cd29491846a5ea01dc7
Tyler Garlick <tjgarlick@gmail.com>
2017-01-18 02:06:28 -0700

    Added hapi

--- src/index.js
+++ src/index.js
@@ -1,7 +1,57 @@
-'use strict';
+import { Server } from 'hapi';
 
-export default {
-  greet(){
-    return 'Hello World';
+const server = new Server({});
+
+const port = process.env.PORT || 4000;
+const env = process.env.NODE_ENV || 'development';
+server.connection({
+  port, router: {
+    isCaseSensitive: false
+  },
+  routes: {
+    cors: true
   }
-}
+});
+
+server.register([
+  require('inert'),
+  require('vision'),
+  require('blipp'),
+  require('tv'),
+  require('hapi-async-handler'),
+  {
+    register: require('hapi-swagger'),
+    options: {
+      cors: true,
+      jsonEditor: true,
+      documentationPath: '/',
+      info: {
+        title: 'Example',
+        version: '1.0.0',
+        description: 'An example api',
+//        contact: {
+//          name: '',
+//          url: '',
+//          email: ''
+//        },
+//        license: {
+//          name: '',
+//          url: ''
+//        }
+      }
+    }
+  }
+], err => {
+  if (err) throw err;
+
+  if (env !== 'testing') {
+    server.start(err => {
+      if (err) throw err;
+      server.log('info', 'Server running at: ' + server.info.uri);
+    });
+  }
+
+});
+
+
+export default server;
