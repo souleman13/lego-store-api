e8cd67fe3b67808f9fe9c627a73b571f52611058
Douglas Soule <souleman13@gmail.com>
2017-07-25 15:51:31 -0600

    created tests
    added tests for crud operations: get put post delete
    basic tests returning true as long as the operations are returning a value.

--- test/test.js
+++ test/test.js
@@ -2,7 +2,31 @@
  * Created by Douglas on 7/21/2017.
  */
 import Test from 'ava';
+import Server from '../src/';
 
 
+Test('/figures returns something', async t => {
+  const res = await Server.inject({ url: '/figures', method: 'GET' });
+  t.truthy(res.result);
+});
 
+Test(`/figures/id returns something`, async t=> {
+  const res = await Server.inject({ url: '/figures/{id}', method: 'GET' });
+  t.truthy(res.result);
+});
+
+Test(`/figures post request returns something`, async t=> {
+  const res = await Server.inject({ url: '/figures', method: 'POST' });
+  t.truthy(res.result);
+});
+
+Test(`/figures/{id} put request returns something`, async t=> {
+  const res = await Server.inject({ url: '/figures/{id}', method: 'PUT' });
+  t.truthy(res.result);
+});
+
+Test(`/figures/{id} delete request returns something`, async t=> {
+  const res = await Server.inject({ url: '/figures/{id}', method: 'DELETE' });
+  t.truthy(res.result);
+});
 
